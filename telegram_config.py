11a06c9b2f531cdb6552d55df07d8fa6091dcd13
gasoftqwerty <163709417+gasoftqwerty@users.noreply.github.com>
2025-11-19 11:51:01 +0200

    telegram_config.py

--- telegram_config.py
+++ telegram_config.py
@@ -1,16 +1,1 @@
-# Конфигурационные данные для Telegram Bot API
-# Токен бота, полученный от BotFather
-#TELEGRAM_BOT_TOKEN = '8055363309:AAGzjEa3Avw-3qgqOGG040XyPxzeNkXAFdg' 
-# Chat ID, полученный из getUpdates НЕ МЕНЯЕТСЯ
-#TELEGRAM_CHAT_ID = 980698898
 
-# telegram_config.py
-# Токен бота, полученный от BotFather
-TELEGRAM_BOT_TOKEN = '8409750391:AAFlAmC1A5MOIzTwXIY7NUqzhiyFrHPWG3Q'
-CITY = "м. Кривий Ріг"
-STREET = "вул. Кропивницького"
-MY_QUEUE = "GPV6.2"
-HOUSE = "39"
-
-# Старый TELEGRAM_CHAT_ID закомментирован, так как теперь мы отправляем всем
-# TELEGRAM_CHAT_ID = 980698898
